c2c2e7cf5660b47b1084e548de2f9828e7cadc2d
Bolesław Muzalewski <boleslaw.muzalewski@lingarogroup.com>
2024-12-10 22:53:45 +0100

    Message - useFormState hook

--- components/Property/PropertyContactForm.jsx
+++ components/Property/PropertyContactForm.jsx
@@ -1,10 +1,55 @@
+"use client";
+
+import { addMessage } from "@/app/actions";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
 import { FaPaperPlane } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const PropertyContactForm = ({ property }) => {
+  const { data: session } = useSession();
+
+  const [state, formAction] = useFormState(addMessage, {});
+
+  useEffect(() => {
+    if (state.error) {
+      toast.error(state.error);
+    }
+
+    if (state.submitted) {
+      toast.success("Message sent successfully");
+    }
+  }, [state]);
+
+  if (!session) {
+    return null;
+  }
+
+  if (state.submitted) {
+    return (
+      <p className="text-green-500 mb-4">
+        Your message has been sent successfully
+      </p>
+    );
+  }
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
       <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
-      <form>
+      <form action={formAction}>
+        <input
+          type="hidden"
+          id="property"
+          name="property"
+          defaultValue={property._id}
+        />
+        <input
+          type="hidden"
+          id="recipient"
+          name="recipient"
+          defaultValue={property.owner}
+        />
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -55,14 +100,14 @@ const PropertyContactForm = ({ property }) => {
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="message"
+            htmlFor="body"
           >
             Message:
           </label>
           <textarea
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
-            id="message"
-            name="message"
+            id="body"
+            name="body"
             placeholder="Enter your message"
           ></textarea>
         </div>
